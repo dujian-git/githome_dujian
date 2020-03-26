086af2763e0818df887b22ef7f6871fdccfebcb1
dujian-git <62688070+dujian-git@users.noreply.github.com>
2020-03-26 14:45:50 +0800

    Add files via upload

---
+++ BONUS.sql
@@ -0,0 +1,21 @@
+
+/****************è¡¨ç»æ****************/
+
+  CREATE TABLE "SCOTT"."BONUS" 
+   (	"ENAME" VARCHAR2(10), 
+	"JOB" VARCHAR2(9), 
+	"SAL" NUMBER, 
+	"COMM" NUMBER
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  TABLESPACE "USERS" 
+
+
+/**************æ¬è¡¨æ³¨é**************/
+
+		æ æ³¨é
+
+
+/**************æ¬è¡¨ç´¢å¼**************/
+
+		æ ç´¢å¼
